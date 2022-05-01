5a10a48924b80991a0eb041b9d8a9568c2eb9b98
Bingo <faaa@live.com>
2022-05-01 11:34:56 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Web
+
+Go Web Common Project Templates
+
+Structure:
+Web
+ - controllers: 处理请求的函数
+ - dao: 数据库的一些操作
+ - logger: 日志
+ - logic: 业务逻辑
+ - models: 墨迹
+ - pkg: 第三方的库
+ - routes: 路由
+ - settings: 配置相关函数
+ - config.yaml: 配置文件
+ - main.go: 主程序
---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"Web/logger"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Setup() *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+	return r
+}
